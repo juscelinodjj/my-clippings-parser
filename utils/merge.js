d40ed59a4be95b4f92b1ec46d556fc9d64d071a5
Juscelino <juscelinodjj@gmail.com>
2022-10-24 14:16:19 -0300

    refactor: replace confuse structure

--- utils/merge.js
+++ utils/merge.js
@@ -1,30 +1,53 @@
 'use strict';
 
-function main(clippings, args) {
-  const {
-    lang, titleAndAuthor, content, title, author, page, position, date, type
-  } = args;
-  clippings.lang = [...new Set([...clippings.lang, lang])];
-  if (title && author) {
-    if (!clippings.library[author]) {
-      clippings.library[author] = {};
-    }
-    if (!clippings.library[author][title]) {
-      clippings.library[author][title] = {count: {}, entries: []};
-    }
-  } else {
-    if (!clippings.library[titleAndAuthor]) {
-      clippings.library[titleAndAuthor] = {count: {}, entries: []};
-    }
+function lang(object, data) {
+  const lang = data.lang;
+  const reference = object['lang'].includes(lang);
+  if (!reference) {
+    object['lang'].push(lang);
   }
-  const reference = author && title
-    ? clippings.library[author][title] : clippings.library[titleAndAuthor];
-  if (!reference['count'][type]) {
-    reference['count'][type] = 0;
+  return object;
+}
+
+function author(object, data) {
+  const {author, title} = data;
+  const reference = object['library'][author ? author : title] ? true : false;
+  if (!reference) {
+    object['library'][author ? author : title] = {};
   }
-  reference['count'][type]++;
-  reference['entries'].push({type, content, page, position, date});
-  return clippings;
+  return object;
+}
+
+function title(object, data) {
+  const {author, title} = data;
+  const reference = object['library'][author][title] ? true : false;
+  if (!reference) {
+    object['library'][author][title] = {'count': {}, 'entries': []};
+  }
+  return object;
+}
+
+function type(object, data) {
+  const {author, title, type} = data;
+  const reference = object['library'][author][title]['count'][type]
+    ? true : false;
+  if (!reference) {
+    object['library'][author][title]['count'][type] = 0;
+  }
+  object['library'][author][title]['count'][type] += 1;
+  return object;
+}
+
+function entry(object, data) {
+  const {author, title, page, position, content, type, date} = data;
+  const entry = {type, content, page, position, date};
+  object['library'][author][title]['entries'].push(entry);
+  return object;
+}
+
+function main(clippings, data) {
+  const fns = [lang, author, title, type, entry];
+  return fns.reduce((accumulator, fn) => fn(accumulator, data), clippings);
 }
 
 export default main;
